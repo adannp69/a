1b3d73f39a62fc75cc66df9fb88bf8c8eff797d4
adannp69 <anunezp1800@alumno.ipn.mx>
2023-06-05 02:51:20 -0600

    1 CSS y HTML

---
+++ HTML/formulario.js
@@ -0,0 +1,18 @@
+/*const correo = document.getElementById("correo");
+const contrasenia = document.getElementById("contrasenia");
+
+function valida
+*/
+
+const expresiones ={
+    nombre_:            /^(?!.*([a-zA-Z])\1\1)[A-Za-zÁ-ÿ]{3,10}(\s[a-zA-Z]{3,10})?/,
+    apellidoPaterno_:   /^(?!.*([A-Za-zÁ-ÿ])\1\1)[A-Za-z]{3,10}/,
+    apellido_Materno_:  /^(?!.*([A-Za-zÁ-ÿ])\1\1)[A-Za-z]{3,10}/,
+    correo_:
+    contrasenia_:
+
+}
+
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');   
+
